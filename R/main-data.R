968f794c973f94ca6b462585773100f1db5f719a
Alex <alexg@DESKTOP-5UTRNGJ.localdomain>
2026-08-12 17:05:33 -0600

    sync

--- R/main-data.R
+++ R/main-data.R
@@ -22,3 +22,46 @@ tc_tpm <- read_tsv("../data/raw/A549_IL1B_Bud_tc_tpm.txt") %>%
     time = factor(time, levels = c(1, 2, 6, 12, 24)),
     treatment = factor(treatment, levels = c("NS", "IL1B", "Bud", "I+B"))
   )
+
+ikba_dea <- read_tsv("../data/raw/a549-ikbadn-ib-timecourse_DEA.txt") %>%
+  mutate(sig = case_when(
+    log2fold <= -1 & FDR <= 0.05 ~ "dn",
+    log2fold >=  1 & FDR <= 0.05 ~ "up",
+    TRUE ~ "ns"
+  )) %>%
+  # group_by(Gene, treatment, condition) %>%
+  # mutate(
+  #   peak = time[which.max(abs(log2fold))],
+  #   peak_sig = sig[which(time == peak)]) %>%
+  # ungroup() %>%
+  mutate(
+    treatment = factor(treatment, levels = c("NS", "IL1B", "Bud", "IB")),
+    condition = factor(condition, levels = c("naive", "Ad-GFP", "Ad-IKBA")),
+    time = factor(time, levels = c(1, 2, 6))
+  )
+
+ikba_tpm <- read_tsv("../data/raw/a549-ikbadn-ib-timecourse_tpm.txt") %>%
+  mutate(
+    treatment = factor(treatment, levels = c("NS", "IL1B", "Bud", "IB")),
+    condition = factor(condition, levels = c("naive", "Ad-GFP", "Ad-IKBA")),
+    time = factor(time, levels = c(1, 2, 6))
+  )
+
+primary_dea = read_tsv("../data/raw/A549vsPrimary_univariateDEA.txt") %>%
+  mutate(
+    sig = case_when(
+      log2fold >= 1 & FDR <= 0.05 ~ "up",
+      log2fold <=-1 & FDR <= 0.05 ~ "dn",
+      TRUE ~ "ns"
+    )
+  ) %>%
+  mutate(
+    celltype = factor(celltype, levels = c("A549", "ALI", "HBE")),
+    treatment = factor(treatment, levels = c("NS", "IL1B", "GC", "combo"))
+  )
+
+primary_tpm = read_tsv("../data/raw/A549vsPrimary_tpm.txt") %>%
+  mutate(
+    celltype = factor(celltype, levels = c("A549", "ALI", "HBE")),
+    treatment = factor(treatment, levels = c("NS", "IL1B", "GC", "combo"))
+  )
