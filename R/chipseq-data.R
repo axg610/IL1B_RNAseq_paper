786c72125a7ed8329b5566a745f97032696c0325
Alex <alexg@alex17.localdomain>
2026-09-06 17:17:42 -0600

    change max distance from 100kB to 30kB

--- R/chipseq-data.R
+++ R/chipseq-data.R
@@ -143,12 +143,12 @@ if(!file.exists("../data/peaks/dba.rds")){
     keep.extra.columns = TRUE
   )
   
-  # ===== find genes roughly within 100 kb =====
+  # ===== find genes roughly within 30 kb =====
   
   hits = findOverlaps(
     peaks_ranges,
     gene_ranges,
-    maxgap = 100000,
+    maxgap = 30000,
     ignore.strand = TRUE
   )
   
@@ -174,7 +174,7 @@ if(!file.exists("../data/peaks/dba.rds")){
     distance_to_TSS = distances
   ) %>%
     filter(grepl("^[A-Z0-9]+$", Gene)) %>%
-    filter(distance_to_TSS <= 100000)
+    filter(distance_to_TSS <= 30000)
   
   peak_gene_summary = peak_gene_map %>%
     group_by(peak_id) %>%
